458f8282b2c4086a63a7c768b526798730c42d9f
olgierdruszniak <s31298@pjwstk.edu.pl>
2025-03-17 09:39:31 +0100

    second commit

--- ConsoleApp1/ConsoleApp1/Container.cs
+++ ConsoleApp1/ConsoleApp1/Container.cs
@@ -6,7 +6,6 @@ public class Container
     private int CargoMass { get; set; }
     private int Height { get; set; }
     private int TareWeight { get; set; }
-    private int CargoWeight { get; set; }
     private int Depth { get; set; }
     private string SerialNumber { get; set; }
     private int MaxPayload { get; set; }
@@ -16,6 +15,22 @@ public class Container
     public Container()
     {
         lastNumber = next_number++;
-        
+    }
+
+    public virtual void EmptyCargo()
+    {
+        CargoMass = 0;
+    }
+
+    public virtual void Load(int CargoMass)
+    {
+        if (CargoMass > MaxPayload)
+        {
+            throw new OverfillException();
+        }
+        else
+        {
+            this.CargoMass = CargoMass;
+        }
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/OverfillException.cs
@@ -0,0 +1,8 @@
+namespace ConsoleApp1;
+
+public class OverfillException : Exception
+{
+    public OverfillException() : base("Overfill exception")
+    {
+    }
+}
